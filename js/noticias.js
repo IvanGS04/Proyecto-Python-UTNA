ae69c1ecc4d65bdde666cf6c89b1bea2f7087762
Ivan <iagsivan15@gmail.com>
2024-03-26 14:01:39 -0600

    Actualizacion 1.1

--- js/noticias.js
+++ js/noticias.js
@@ -1,14 +1,14 @@
 document.querySelectorAll('.leer-mas').forEach(button => {
-    button.addEventListener('click', function() {
-      const card = this.closest('.card');
-      const imagen = card.querySelector('.card-img-top').src;
-      const titulo = card.querySelector('.card-title').textContent;
-      const descripcion = this.getAttribute('data-descripcion');
-
-      document.getElementById('noticiaModalLabel').textContent = titulo;
-      document.getElementById('noticiaImagen').src = imagen;
-      document.getElementById('noticiaTexto').textContent = descripcion;
-
-      $('#noticiaModal').modal('show');
-    });
+  button.addEventListener('click', function() {
+    const card = this.closest('.card');
+    const imagen = card.querySelector('.card-img-top').src;
+    const titulo = card.querySelector('.card-title').textContent;
+    const descripcion = this.getAttribute('data-descripcion');
+    
+    document.getElementById('noticiaModalLabel').textContent = titulo;
+    document.getElementById('noticiaImagen').src = imagen;
+    document.getElementById('noticiaTexto').textContent = descripcion;
+    
+    $('#noticiaModal').modal('show');
   });
+});
